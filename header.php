5a084501842634fb4823c420efebff1daced065d
tKlancelot <princesuper42@gmail.com>
2022-10-24 02:57:33 +0200

    amelioration des themes + titres
    ok

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <meta name="description" content="Portfolio de Tarik Louatah, développeur web front-end basé à Paris, intégrateur et concepteur d'interfaces web.">
       <link rel="icon" href="<?= get_template_directory_uri() . '/assets/favicon/fav.png' ?>" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.min.js" integrity="sha512-n8IpKWzDnBOcBhRlHirMZOUvEq2bLRMuJGjuVqbzUJwtTsgwOgK5aS0c1JA647XWYfqvXve8k3PtZdzpipFjgg==" crossorigin="anonymous"></script>
       <script src="https://unpkg.com/three@0.126.0/examples/js/loaders/GLTFLoader.js"></script>
